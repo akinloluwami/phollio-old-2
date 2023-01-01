12c995ba048481f253128a4ce571a09c898401d0
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 23:44:03 +0100

    initial commit

---
+++ components/AddNewLinkModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddNewLinkModal = () => {
+  return <div className="h-44 w-96 bg-accent rounded absolute top-0">AddNewLinkModal</div>;
+};
+
+export default AddNewLinkModal;
