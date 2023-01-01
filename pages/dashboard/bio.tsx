1ea6834c1c8929434e96d7c4e8fddae4684952a6
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-02 00:38:30 +0100

    styled input fields

--- pages/dashboard/bio.tsx
+++ pages/dashboard/bio.tsx
@@ -8,14 +8,22 @@ const Bio = () => {
       <div className="w-full">
         <ProfilePicture />
       </div>
-      <div className="w-full">
-        <div className="w-full">
+      <div className="w-full shadow p-3 my-10">
+        <div className="w-full my-2">
           <p>Profile name</p>
-          <input type="text" placeholder="Display name" />
+          <input
+            type="text"
+            placeholder="Display name"
+            className="w-full h-10 pl-3 border-[1px] border-accent"
+          />
         </div>
-        <div className="">
+        <div className="my-2">
           <p>Profile description</p>
-          <textarea placeholder="Bio" maxLength={300} />
+          <textarea
+            placeholder="Bio"
+            maxLength={300}
+            className="w-full h-32 py-2 pl-3 border-[1px] border-accent"
+          />
         </div>
       </div>
     </DashboardLayout>
