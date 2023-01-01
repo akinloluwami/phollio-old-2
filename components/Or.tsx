5cff78b01563a22e4f95ffec38d54c5725c211c5
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 20:29:13 +0100

    initial commit

---
+++ components/Or.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Or = () => {
+  return (
+    <div className="flex items-center mt-3 justify-between">
+      <div className="h-[2px] w-48 bg-accent"></div>
+      <p className="px-2">Or</p>
+      <div className="h-[2px] w-48 bg-accent"></div>
+    </div>
+  );
+};
+
+export default Or;
