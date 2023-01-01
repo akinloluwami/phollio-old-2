e1062f84fb9d46e742ce0cde1acdfdb1e6e09397
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 20:30:52 +0100

    initial commit

---
+++ pages/dashboard/projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Projects = () => {
+  return (
+    <DashboardLayout>
+      <div className="">
+        <h1>PROJECTSSSSSSSSSSS</h1>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Projects;
