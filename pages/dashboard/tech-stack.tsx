e063c64c2b32e507b33b22e906df926a2f112b80
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 20:31:01 +0100

    initial commit

---
+++ pages/dashboard/tech-stack.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const TechStack = () => {
+  return (
+    <DashboardLayout>
+      <div className="">
+        <h1>TSSSSSSSSSSSSSSSSSSSSSSSSSSSSS</h1>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default TechStack;
