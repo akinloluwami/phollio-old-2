d955b988d59df2fc003c078581632b06468df38a
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 20:30:34 +0100

    initial commit

---
+++ pages/dashboard/bio.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Bio = () => {
+  return (
+    <DashboardLayout>
+      <div className="">
+        <h1>BIOOOOOOOOOOOOOOOOOOO</h1>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Bio;
