36610910ed1b120b47a832284bd8243c05f82144
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-02 00:31:29 +0100

    added input fields

--- pages/dashboard/bio.tsx
+++ pages/dashboard/bio.tsx
@@ -8,6 +8,16 @@ const Bio = () => {
       <div className="w-full">
         <ProfilePicture />
       </div>
+      <div className="w-full">
+        <div className="w-full">
+          <p>Profile name</p>
+          <input type="text" placeholder="Display name" />
+        </div>
+        <div className="">
+          <p>Profile description</p>
+          <textarea placeholder="Bio" maxLength={300} />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
