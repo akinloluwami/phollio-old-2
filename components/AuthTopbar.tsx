488d8bceb7ad5f973aa190f29a92ba4d730e8d20
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-01 20:28:28 +0100

    initial commit

---
+++ components/AuthTopbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+interface CompTypes {
+  question: string;
+  href: string;
+  hrefText: string;
+}
+
+const AuthTopbar = ({ question, href, hrefText }: CompTypes) => {
+  return (
+    <div className="flex justify-between px-20 py-4">
+      <Link href={"/"}>
+        <h1>YouPage</h1>
+      </Link>
+      <div className="flex items-center">
+        <p className="mr-5 font-semibold">{question}</p>
+        <Link href={`/${href}`}>
+          <button className="bg-accent text-white px-7 py-2 font-bold">
+            {hrefText}
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AuthTopbar;
