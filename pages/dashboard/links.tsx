cf3958719804b9f5104d9ef8eb5dfef748a53b3f
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-02 00:12:13 +0100

    added conditional rendering

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import AddNewLinkModal from "../../components/AddNewLinkModal";
+import { useState } from "react";
+import AddNewLinkComp from "../../components/AddNewLinkComp";
 import LinkCard from "../../components/LinkCard";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Links = () => {
+  const [showAddLinkComp, setShowAddLinkComp] = useState(false);
   return (
     <DashboardLayout>
       <div className="w-full">
-        <AddNewLinkModal />
-
-        <button className="w-full h-10 bg-accent text-lg text-white my-2 font-bold">
-          Add Link
+        <button
+          className="w-full h-10 bg-accent text-lg text-white my-2 font-bold"
+          onClick={() => setShowAddLinkComp(!showAddLinkComp)}
+        >
+          {showAddLinkComp ? "Close" : "Add New Link"}
         </button>
+        {showAddLinkComp && <AddNewLinkComp />}
         <div className="w-full">
           <LinkCard />
           <LinkCard />
