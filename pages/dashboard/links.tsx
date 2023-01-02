b4911a6411f899088bf6799dcad54e2a864483dd
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-02 09:34:13 +0100

    renamed state

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -4,17 +4,17 @@ import LinkCard from "../../components/LinkCard";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Links = () => {
-  const [showAddLinkComp, setShowAddLinkComp] = useState(false);
+  const [show, setShow] = useState(false);
   return (
     <DashboardLayout>
       <div className="w-full">
         <button
           className="w-full h-10 bg-accent text-lg text-white my-2 font-bold"
-          onClick={() => setShowAddLinkComp(!showAddLinkComp)}
+          onClick={() => setShow(!show)}
         >
-          {showAddLinkComp ? "Close" : "Add New Link"}
+          {show ? "Close" : "Add New Link"}
         </button>
-        {showAddLinkComp && <AddNewLinkComp />}
+        {show && <AddNewLinkComp />}
         <div className="w-full">
           <LinkCard />
           <LinkCard />
