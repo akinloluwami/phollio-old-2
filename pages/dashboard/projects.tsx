8078ef4b6bf545bd5fcc0db6e3df51cad3ad6d3a
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-02 09:34:38 +0100

    added page elements

--- pages/dashboard/projects.tsx
+++ pages/dashboard/projects.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import ProjectCard from "../../components/ProjectCard";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Projects = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <DashboardLayout>
-      <div className="">
-        <h1>PROJECTSSSSSSSSSSS</h1>
+      <div className="w-full">
+        <button
+          className="w-full h-10 bg-accent text-lg text-white my-2 font-bold"
+          onClick={() => setShow(!show)}
+        >
+          {show ? "Close" : "Add New Project"}
+        </button>
+        <div className="w-full">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </div>
     </DashboardLayout>
   );
