237b361ee41b805670c21b6c92964a94ea1bfb28
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-03 22:08:02 +0100

    added style for "disabled"

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -69,7 +69,7 @@ const Signup = () => {
                   />
                 </div>
                 <button
-                  className="w-full bg-accent pl-3 text-lg py-3 text-white mt-8 font-bold"
+                  className="w-full bg-accent pl-3 text-lg py-3 text-white mt-8 font-bold disabled:opacity-80 disabled:cursor-not-allowed"
                   onClick={() => setCurrentStep(2)}
                 >
                   Continue
