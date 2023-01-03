a2ea770c410a0cef019b87e52910803c3227ce15
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-03 17:04:26 +0100

    initial commit

---
+++ utils/requests.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+//add the default url
+let defaultUrl: string = "http://localhost:2004";
+
+const postData = async (
+  url: string,
+  payload?: any,
+  configurations?: any
+): Promise<any> => {
+  try {
+    const response = await axios.post(
+      `${defaultUrl}${url}`,
+      payload,
+      configurations
+    );
+    return response;
+  } catch (error: any) {
+    return error.response;
+  }
+};
+
+const fetchData = async (
+  url?: string,
+  payload?: any,
+  configurations?: any
+): Promise<any> => {
+  try {
+    const response = await axios.get(`${defaultUrl}${url}`, payload);
+    return response;
+  } catch (error: any) {
+    const err = error.response;
+    return error;
+  }
+};
+
+const deleteData = async (url: string, payload: any): Promise<any> => {
+  try {
+    const response = await axios.delete(`${defaultUrl}${url}`, payload);
+    return response;
+  } catch (error: any) {
+    const err = error.response;
+    return error;
+  }
+};
+
+const putData = async (
+  url: string,
+  payload: any,
+  configurations?: any
+): Promise<any> => {
+  try {
+    const response = await axios.put(
+      `${defaultUrl}${url}`,
+      payload,
+      configurations
+    );
+    return response;
+  } catch (error: any) {
+    return error.response;
+  }
+};
+
+export { postData, fetchData, deleteData, putData };
