7637b5e3ffb16ed3aecc263d0cf77a0220038125
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-04 14:23:22 +0100

    working...

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -6,6 +6,9 @@ import AuthLayout from "../layouts/AuthLayout";
 import React, { useEffect, useState } from "react";
 import { fetchData } from "../utils/requests";
 import { debounce } from "lodash";
+import { BsCheckAll } from "react-icons/bs";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { RiLoader3Fill } from "react-icons/ri";
 
 const Signup = () => {
   const [currentStep, setCurrentStep] = useState(1);
@@ -15,17 +18,26 @@ const Signup = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
 
-  const [emailChecked, setEmailChecked] = useState(false);
-  const [usernameChecked, setUsernameChecked] = useState(false);
+  const [emailAvaibable, setEmailAvailable] = useState(false);
   const [isCheckingEmail, setIsCheckingEmail] = useState(false);
   const [emailCheckMsg, setEmailCheckMsg] = useState("");
+  const [usernameAvaibable, setUsernameAvailable] = useState(false);
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [usernameCheckMsg, setUsernameCheckMsg] = useState("");
 
   const checkEmail = () => {
     setIsCheckingEmail(true);
     const payload = { email };
     fetchData("/auth/check-email", payload).then((data) => {
-      const res = data.response;
+      const res = data;
       console.log(res);
+      if (res.status !== 200) {
+        setEmailAvailable(false);
+        setEmailCheckMsg(res.data.message);
+      } else {
+        setEmailAvailable(true);
+        setEmailCheckMsg(res.data.message);
+      }
       setIsCheckingEmail(false);
     });
   };
@@ -35,8 +47,31 @@ const Signup = () => {
     checkEmail();
   }, 500);
 
+  const checkUsename = () => {
+    setIsCheckingUsername(true);
+    const payload = { username };
+    fetchData("/auth/check-username", payload).then((data) => {
+      const res = data;
+      console.log(res);
+      if (res.status !== 200) {
+        setUsernameAvailable(false);
+        setUsernameCheckMsg(res.data.message);
+      } else {
+        setUsernameAvailable(true);
+        setUsernameCheckMsg(res.data.message);
+      }
+      setIsCheckingUsername(false);
+    });
+  };
+
+  const debouncedCheckUsername = debounce((e) => {
+    setUsername(e.target.value);
+    checkUsename();
+  }, 500);
+
   useEffect(() => {
     checkEmail();
+    checkUsename();
   }, []);
 
   return (
@@ -60,9 +95,32 @@ const Signup = () => {
                     className="w-full bg-blue-50 pl-3 text-lg py-3"
                     placeholder="example@mail.com"
                     onChange={debouncedCheckEmail}
-                    // value={email}
+                    value={email}
                   />
-                  {isCheckingEmail && <small>I'm checking...</small>}
+                  {isCheckingEmail && (
+                    <div className="flex items-center text-accent my-2">
+                      <p className="text-xl animate-spin">
+                        <RiLoader3Fill />
+                      </p>
+                      <small>Checking...</small>
+                    </div>
+                  )}
+                  {!emailAvaibable && (
+                    <div className="flex items-center text-red-500 my-2">
+                      <p className="text-xl">
+                        <IoCloseCircleOutline />
+                      </p>
+                      <small>{emailCheckMsg}</small>
+                    </div>
+                  )}
+                  {emailAvaibable && (
+                    <div className="flex items-center text-green-500 my-2">
+                      <p className="text-xl">
+                        <BsCheckAll />
+                      </p>
+                      <small>{emailCheckMsg}</small>
+                    </div>
+                  )}
                 </div>
                 <div className="mt-6 mb-1">
                   <p className="mb-2 text-lg">Choose a username</p>
@@ -70,12 +128,36 @@ const Signup = () => {
                     type="text"
                     className="w-full bg-blue-50 pl-3 text-lg py-3"
                     placeholder="username"
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={debouncedCheckUsername}
                     value={username}
                   />
+                  {isCheckingUsername && (
+                    <div className="flex items-center text-accent my-2">
+                      <p className="text-xl animate-spin">
+                        <RiLoader3Fill />
+                      </p>
+                      <small>Checking...</small>
+                    </div>
+                  )}
+                  {!usernameAvaibable && (
+                    <div className="flex items-center text-red-500 my-2">
+                      <p className="text-xl">
+                        <IoCloseCircleOutline />
+                      </p>
+                      <small>{usernameCheckMsg}</small>
+                    </div>
+                  )}
+                  {usernameAvaibable && (
+                    <div className="flex items-center text-green-500 my-2">
+                      <p className="text-xl">
+                        <BsCheckAll />
+                      </p>
+                      <small>{usernameCheckMsg}</small>
+                    </div>
+                  )}
                 </div>
                 <button
-                  disabled={!usernameChecked || !emailChecked}
+                  disabled={!usernameAvaibable || !emailAvaibable}
                   className="w-full bg-accent pl-3 text-lg py-3 text-white mt-8 font-bold disabled:opacity-50 disabled:cursor-not-allowed"
                   onClick={() => setCurrentStep(2)}
                 >
