c33a53e9e426212bb146329de652ff9e039e8e30
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-06 12:25:58 +0100

    removed unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { store } from "../contexts/store";
 import { UserProvider } from "../contexts/userContext";
 
 export default function App({ Component, pageProps }: AppProps) {
