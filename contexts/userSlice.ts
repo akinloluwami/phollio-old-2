12ca2f8503e27e1dc5daa6fd46479916b97cdf55
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-06 11:31:24 +0100

    user slice

---
+++ contexts/userSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface UserState {
+  token: string;
+  username: string;
+  isEmailVerified: boolean;
+}
+
+const initialState: UserState = {
+  token: "11111111111111111",
+  username: "prdfhjkoiytgbnkiuytrfghjitfgh",
+  isEmailVerified: false,
+};
+
+export const fetchUserData = createAsyncThunk("user/fetchData", async () => {
+  const response = await axios.get<UserState>("/api/user");
+  return response.data;
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUsername(state, action: PayloadAction<string>) {
+      state.username = action.payload;
+    },
+    setIsEmailVerified(state, action: PayloadAction<boolean>) {
+      state.isEmailVerified = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchUserData.fulfilled]: (
+      state: any,
+      action: PayloadAction<UserState>
+    ) => {
+      state.token = action.payload.token;
+      state.username = action.payload.username;
+      state.isEmailVerified = action.payload.isEmailVerified;
+    },
+  },
+});
+
+export const { setUsername, setIsEmailVerified } = userSlice.actions;
+export default userSlice.reducer;
