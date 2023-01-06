3bfdf89a54004d893b47c87d901356d184f3cf3a
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-06 12:15:32 +0100

    provided the user context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../contexts/store";
+import { UserProvider } from "../contexts/userContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <UserProvider>
       <Component {...pageProps} />
-    </Provider>
+    </UserProvider>
   );
 }
