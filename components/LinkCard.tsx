0e4a2cc79a188b03125ee8e8597a5dc230dd6a9d
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-09 20:15:52 +0100

    ...

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ToggleSwitch from "./ToggleSwitch";
 import { HiEye, HiOutlineTrash } from "react-icons/hi2";
 import { TbClick } from "react-icons/tb";
-import { postData } from "../utils/requests";
+import { deleteData, postData } from "../utils/requests";
 import { useUser } from "../contexts/userContext";
 
 const LinkCard = ({ title, url, impressions, clicks, isOn, id }: any) => {
@@ -16,6 +16,19 @@ const LinkCard = ({ title, url, impressions, clicks, isOn, id }: any) => {
       }
     );
   };
+
+  const DeleteLink = () => {
+    deleteData(
+      "/link",
+      { linkId: id },
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    ).then((data) => {
+      console.log(data);
+    });
+  };
+
   return (
     <div className="w-full px-4 bg-white shadow-sm py-5 my-10 flex items-center justify-between">
       <div className="">
@@ -36,7 +49,7 @@ const LinkCard = ({ title, url, impressions, clicks, isOn, id }: any) => {
         <button>
           <ToggleSwitch ToggleLink={ToggleLink} isOn={isOn} />
         </button>
-        <button className="text-xl mt-5">
+        <button className="text-xl mt-5" onClick={DeleteLink}>
           <HiOutlineTrash />
         </button>
       </div>
