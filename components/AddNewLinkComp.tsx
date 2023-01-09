19da52bf4a06fe3d99681faf059bbc0b16433524
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-09 14:23:51 +0100

    ...

--- components/AddNewLinkComp.tsx
+++ components/AddNewLinkComp.tsx
@@ -24,7 +24,7 @@ const AddNewLinkComp = () => {
       setLoading(false);
       if (data.status !== 200) {
         setError(true);
-        setErrorMsg(data.data.message);
+        setErrorMsg(data.data.message || data.data.error);
       } else {
         setSuccess(true);
         setSuccessMsg(data.data.message);
@@ -33,41 +33,43 @@ const AddNewLinkComp = () => {
   };
 
   return (
-    <div className="h-44 w-full pt-3 my-5 bg-gray-100">
+    <>
       {error && !loading && !success && (
         <p className="text-red-500 text-center my-5">{errorMsg}</p>
       )}
       {success && !loading && (
         <p className="text-green-500 text-center my-5">{successMsg}</p>
       )}
-      <div className="px-5">
-        <div className="my-3">
-          <p>Link title</p>
-          <input
-            type="text"
-            placeholder="My Twitter"
-            className="w-full h-10 pl-3"
-            onChange={(e) => setTitle(e.target.value)}
-          />
+      <div className="h-44 w-full pt-3 my-5 bg-gray-100">
+        <div className="px-5">
+          <div className="my-3">
+            <p>Link title</p>
+            <input
+              type="text"
+              placeholder="My Twitter"
+              className="w-full h-10 pl-3"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="my-3">
+            <p>Link URL</p>
+            <input
+              type="text"
+              className="w-full h-10 pl-3"
+              placeholder="https://twitter.com/xing0x"
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="my-3">
-          <p>Link URL</p>
-          <input
-            type="text"
-            className="w-full h-10 pl-3"
-            placeholder="https://twitter.com/xing0x"
-            onChange={(e) => setUrl(e.target.value)}
-          />
-        </div>
+        <button
+          className="w-full h-10 text-white bg-accent disabled:cursor-not-allowed disabled:opacity-50 transition-opacity"
+          disabled={!title || !url || loading}
+          onClick={handleSubmit}
+        >
+          Save
+        </button>
       </div>
-      <button
-        className="w-full h-10 bg-gray-300 text-accent hover:text-white hover:bg-accent transition-colors"
-        disabled={!title || !url || loading}
-        onClick={handleSubmit}
-      >
-        Save
-      </button>
-    </div>
+    </>
   );
 };
 
