311ff865e4784a684cd7fb1dbf0b422d116e58ee
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-09 16:18:31 +0100

    replaced the dummy data with the real data

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -7,16 +7,16 @@ const LinkCard = ({ title, url, impressions, clicks }: any) => {
   return (
     <div className="w-full px-4 bg-white shadow-sm py-5 my-10 flex items-center justify-between">
       <div className="">
-        <h1 className="font-semibold text-lg">Link title</h1>
-        <h2 className="font-medium text-gray-600">linktitle.com</h2>
+        <h1 className="font-semibold text-lg">{title}</h1>
+        <h2 className="font-medium text-gray-600">{url}</h2>
         <div className="flex items-center gap-4 my-2 text-gray-600">
           <div className="flex items-center gap-[4px]">
             <HiEye />
-            1.1k
+            {impressions}
           </div>
           <div className="flex items-center gap-[4px]">
             <TbClick />
-            100
+            {clicks}
           </div>
         </div>
       </div>
