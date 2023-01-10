d09d9e49c1873a96304b0f268093474f15d768b5
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-10 19:29:22 +0100

    added description field to project schema

---
+++ backend/src/schema/project.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const linkSchema = new Schema({
+  title: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  userId: {
+    type: String,
+  },
+  isOn: {
+    type: Boolean,
+    default: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Link = mongoose.model("Link", linkSchema);
+
+export default Link;
