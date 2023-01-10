4decd6db479c96a5028050db4cc456b4d0e975c6
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-10 20:01:47 +0100

    added getLinks function

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,5 +1,6 @@
 import addLink from "./link/add-new-link";
 import toggleLink from "./link/toggle-link";
 import deleteLink from "./link/delete-link";
+import getLinks from "./link/get-links";
 
-export { addLink, toggleLink, deleteLink };
+export { addLink, toggleLink, deleteLink, getLinks };
