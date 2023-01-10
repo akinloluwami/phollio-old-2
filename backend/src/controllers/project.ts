d896a7a0a9343cca897198f81780a09c6ef6790c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-10 20:02:02 +0100

    added getProjects fucntion

--- backend/src/controllers/project.ts
+++ backend/src/controllers/project.ts
@@ -1,5 +1,6 @@
 import addProject from "./project/add-new-project";
 import toggleProject from "./project/toggle-project";
 import deleteProject from "./project/delete-project";
+import getProjects from "./project/get-projects";
 
-export { addProject, toggleProject, deleteProject };
+export { addProject, toggleProject, deleteProject, getProjects };
