7d1adc5f5be4d62bc9ba44260b6a84ce643e0ef5
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-10 19:58:14 +0100

    added project routes

---
+++ backend/src/controllers/project.ts
@@ -0,0 +1,5 @@
+import addProject from "./project/add-new-project";
+import toggleProject from "./project/toggle-project";
+import deleteProject from "./project/delete-project";
+
+export { addProject, toggleProject, deleteProject };
