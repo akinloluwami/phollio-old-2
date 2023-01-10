0e0d92bc4307a48bc8cadf7f5429ee6dccb6e321
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-10 20:08:54 +0100

    remaned function

--- backend/src/controllers/project/delete-project.ts
+++ backend/src/controllers/project/delete-project.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import Link from "../../schema/link";
+import Project from "../../schema/link";
 import user from "../../schema/user";
 
-const deleteLink = async (req: Request, res: Response) => {
+const deleteProject = async (req: Request, res: Response) => {
   try {
     if (!req.headers.authorization) {
       return res.status(401).json({ message: "Unauthorized" });
@@ -18,18 +18,18 @@ const deleteLink = async (req: Request, res: Response) => {
     }
 
     const { linkId } = req.query;
-    const link = await Link.findOne({ _id: linkId, userId });
+    const link = await Project.findOne({ _id: linkId, userId });
     if (!link) {
-      return res.status(400).json({ message: "Link does not exist" });
+      return res.status(400).json({ message: "Project does not exist" });
     }
 
     link.isDeleted = true;
     await link.save();
 
-    res.status(200).json({ message: "Link deleted successfully" });
+    res.status(200).json({ message: "Project deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error deleting link", error });
   }
 };
 
-export default deleteLink;
+export default deleteProject;
