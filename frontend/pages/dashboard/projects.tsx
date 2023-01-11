6bc3fc7bac66d812a1a002c4d5610017744c1686
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-11 22:46:49 +0100

    .

--- frontend/pages/dashboard/projects.tsx
+++ frontend/pages/dashboard/projects.tsx
@@ -3,6 +3,7 @@ import NewProject from "../../components/NewProject";
 import ProjectCard from "../../components/ProjectCard";
 import { useUser } from "../../contexts/userContext";
 import DashboardLayout from "../../layouts/DashboardLayout";
+import ProjectProps from "../../types/project";
 import { fetchData } from "../../utils/requests";
 
 const Projects = () => {
@@ -22,7 +23,7 @@ const Projects = () => {
       if (data.status !== 200) {
         setErrorMsg(data.data.message);
       } else {
-        setProjects(data.data.links);
+        setProjects(data.data.projects);
       }
     });
   };
@@ -42,14 +43,29 @@ const Projects = () => {
         </button>
         {show && <NewProject />}
         <div className="w-full">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {projects.length < 1 ? (
+            <div className="my-3">
+              <h1 className="font-semibold text-center text-3xl">
+                No projects
+              </h1>
+              <p className="text-center">
+                Click the button above to add your first project
+              </p>
+            </div>
+          ) : (
+            projects.map((project: ProjectProps) => (
+              <ProjectCard
+                key={project.id}
+                id={project.id}
+                title={project.title}
+                url={project.url}
+                description={project.description}
+                impressions={project.impressions}
+                clicks={project.clicks}
+                isOn={project.isOn}
+              />
+            ))
+          )}
         </div>
       </div>
     </DashboardLayout>
