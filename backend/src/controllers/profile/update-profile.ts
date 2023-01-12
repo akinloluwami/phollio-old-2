9a9f5ef6cd1200154f00f8f74e7b4a7e4446a881
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-12 23:11:54 +0100

    fix

--- backend/src/controllers/profile/update-profile.ts
+++ backend/src/controllers/profile/update-profile.ts
@@ -15,29 +15,30 @@ const updateProfile = async (req: Request, res: Response) => {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
     const userId = decoded.uuid;
 
-    const { username, email, bio } = req.body;
+    const { username, displayName, bio } = req.body;
     const user = await User.User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    if (username) {
-      if (!/^[a-zA-Z0-9]+$/.test(username)) {
-        return res.status(400).json({
-          message: "Username can only contain letters and numbers",
-        });
-      }
-      user.username = username;
+    console.log({ username, displayName, bio });
+    if (!username || !displayName) {
+      return res
+        .status(400)
+        .json({ message: "Username and display name cannot be empty" });
     }
-    if (email) {
-      if (!validator.isEmail(email)) {
-        return res.status(400).json({ message: "Invalid email" });
-      }
-      user.email = email;
+
+    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+      return res.status(400).json({
+        message: "Username can only contain letters and numbers",
+      });
     }
-    if (bio) {
-      user.bio = bio;
-    }
+
+    user.username = username;
+    user.displayName = displayName;
+    user.bio = bio;
+
     await user.save();
+
     return res.status(200).json({ message: "Profile updated successfully" });
   } catch (error) {
     return res.status(500).json({ message: "Error updating profile", error });
