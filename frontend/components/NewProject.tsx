39a1ec149c86ea7de649d5904582273b0adb51e2
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-12 12:47:43 +0100

    added success and error messages

--- frontend/components/NewProject.tsx
+++ frontend/components/NewProject.tsx
@@ -37,47 +37,57 @@ const NewProject = () => {
   };
 
   return (
-    <div className="w-full  p-3 ">
-      <div className="w-full my-5">
-        <p>Project title</p>
-        <input
-          type="text"
-          placeholder="Name of your project"
-          className="w-full h-10 pl-3 border-[1px] bg-gray-50"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div className="my-2">
-        <p>Project description</p>
-        <textarea
-          placeholder="What's your project about?"
-          maxLength={150}
-          className="w-full h-24 py-2 pl-3 border-[1px] bg-gray-50"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <div className="w-full my-2">
-        <p>Project URL</p>
-        <input
-          type="text"
-          placeholder="Your project URL"
-          className="w-full h-10 pl-3 border-[1px] bg-gray-50"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-      </div>
-      <button
-        className="w-full h-10 text-white
+    <>
+      {error && !loading && !success && (
+        <p className="text-red-500 text-center my-5">{errorMsg}</p>
+      )}
+
+      {success && !loading && (
+        <p className="text-green-500 text-center my-5">{successMsg}</p>
+      )}
+
+      <div className="w-full  p-3 ">
+        <div className="w-full my-5">
+          <p>Project title</p>
+          <input
+            type="text"
+            placeholder="Name of your project"
+            className="w-full h-10 pl-3 border-[1px] bg-gray-50"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="my-2">
+          <p>Project description</p>
+          <textarea
+            placeholder="What's your project about?"
+            maxLength={150}
+            className="w-full h-24 py-2 pl-3 border-[1px] bg-gray-50"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="w-full my-2">
+          <p>Project URL</p>
+          <input
+            type="text"
+            placeholder="Your project URL"
+            className="w-full h-10 pl-3 border-[1px] bg-gray-50"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </div>
+        <button
+          className="w-full h-10 text-white
         bg-accent disabled:cursor-not-allowed disabled:opacity-50 transition-opacity
         "
-        disabled={!title || !url || !description || loading}
-        onClick={handleSubmit}
-      >
-        Publish
-      </button>
-    </div>
+          disabled={!title || !url || !description || loading}
+          onClick={handleSubmit}
+        >
+          Publish
+        </button>
+      </div>
+    </>
   );
 };
 
