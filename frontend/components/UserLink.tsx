5ef7b0d86454ecdcddb29c898c2aa0c41fee4df8
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-13 12:09:24 +0100

    put username in  a link

--- frontend/components/UserLink.tsx
+++ frontend/components/UserLink.tsx
@@ -1,5 +1,9 @@
 import { useUser } from ".././contexts/userContext";
 const UserLink = () => {
   const { username } = useUser();
-  return <></>;
+  return (
+    <div className="flex items-center gap-2">
+      <a href={`http://localhost:1000/${username}`}>phollio.com/${username}</a>
+    </div>
+  );
 };
