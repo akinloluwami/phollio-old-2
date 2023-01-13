c4f68c4310d3a45984c5a1d2208bb50ea1f77646
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-13 13:35:23 +0100

    ...

--- frontend/components/UserLink.tsx
+++ frontend/components/UserLink.tsx
@@ -1,8 +1,10 @@
 import { useUser } from ".././contexts/userContext";
 import { MdContentCopy } from "react-icons/md";
 import useCopyToClipboard from "../hooks/useCopyToClipboard";
+import { useState } from "react";
 const UserLink = () => {
   const { username } = useUser();
+  const [copySt, setCopySt] = useState("Copy link");
   const [value, copy] = useCopyToClipboard();
   const link = `http://localhost:1000/${username}`;
   return (
@@ -11,8 +13,17 @@ const UserLink = () => {
         phollio.com/{username}
       </a>
 
-      <button onClick={() => copy(link)}>
-        <MdContentCopy />
+      <button
+        onClick={() => {
+          copy(link);
+          setCopySt("Copied");
+          setTimeout(() => {
+            setCopySt("Copy link");
+          }, 1500);
+        }}
+        className="p-1 transition-colors hover:bg-[rgba(0,0,0,0.2)] flex items-center text-sm"
+      >
+        <MdContentCopy /> {copySt}
       </button>
     </div>
   );
