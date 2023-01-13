922873027c0b28bce71c130be2a6d8d976de2ed9
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-13 12:22:03 +0100

    removed unnecessary dollar sign

--- frontend/components/UserLink.tsx
+++ frontend/components/UserLink.tsx
@@ -3,7 +3,7 @@ const UserLink = () => {
   const { username } = useUser();
   return (
     <div className="flex items-center gap-2">
-      <a href={`http://localhost:1000/${username}`}>phollio.com/${username}</a>
+      <a href={`http://localhost:1000/${username}`}>phollio.com/{username}</a>
     </div>
   );
 };
