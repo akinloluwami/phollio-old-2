28537be89918be635623e871aad37badb92a5bd9
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-13 12:25:22 +0100

    added copy button

--- frontend/components/UserLink.tsx
+++ frontend/components/UserLink.tsx
@@ -1,9 +1,14 @@
 import { useUser } from ".././contexts/userContext";
+import { MdContentCopy } from "react-icons/md";
 const UserLink = () => {
   const { username } = useUser();
   return (
     <div className="flex items-center gap-2">
       <a href={`http://localhost:1000/${username}`}>phollio.com/{username}</a>
+
+      <button>
+        <MdContentCopy />
+      </button>
     </div>
   );
 };
