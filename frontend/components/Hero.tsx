f2618ae044fd41f895cb735a42577a40939cdf2c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-14 21:01:12 +0100

    absolute

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
   }, 500);
   return (
     <div className="bg-white h-screen w-screen text-black relative">
-      <div className="lg:hidden">
+      <div className="lg:hidden absolute">
         <ul className="font-bold text-xl">
           <li>
             <a href="#">Explore Developers</a>
