d951ac91dd83360f116a5144ef559f65f31ac88c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-15 12:58:47 +0100

    a

--- backend/src/controllers/profile.ts
+++ backend/src/controllers/profile.ts
@@ -1,3 +1,4 @@
 import getBasicInfo from "./profile/basic-info";
 import updateProfile from "./profile/update-profile";
-export { getBasicInfo, updateProfile };
+import uploadProfilePicture from "./profile/profile-picture";
+export { getBasicInfo, updateProfile, uploadProfilePicture };
