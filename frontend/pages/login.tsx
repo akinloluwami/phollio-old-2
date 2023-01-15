b7e0d168d321cd7d70fd67ef7cd6bece7c7070c2
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-15 11:55:47 +0100

    @!

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -40,7 +40,9 @@ const Login = () => {
       <AuthTopbar href="signup" hrefText="Sign up" question="New to YouPage?" />
       <AuthLayout>
         <div className="lg:w-1/3 w-11/12 mx-auto">
-          <h1 className="text-4xl font-bold text-center">Login</h1>
+          <h1 className="text-4xl font-bold text-center lg:mt-2 mt-10">
+            Login
+          </h1>
           {error && !loading && !success && (
             <p className="text-red-500 text-center my-5">{errorMsg}</p>
           )}
