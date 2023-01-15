f2c38823cea7dd98d3fdd34bd4448a4ad82567d7
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-15 12:42:38 +0100

    config

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: process.env.CD_NAME,
+  api_key: process.env.CD_API_KEY,
+  api_secret: process.env.CD_API_SECRET,
 });
