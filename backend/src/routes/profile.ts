bbe334d4bb7a5446614f21d3a55890ae9e2923b5
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-15 12:59:24 +0100

    dp

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getBasicInfo, updateProfile } from "../controllers/profile";
+import {
+  getBasicInfo,
+  updateProfile,
+  uploadProfilePicture,
+} from "../controllers/profile";
 
 const router = express.Router();
 
 router.get("/basic-info", getBasicInfo);
 router.post("/update", updateProfile);
+router.post("/profile-picture", uploadProfilePicture);
 
 export default router;
