06dc3139cf91b8af85cab0294fd85186d488e915
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-15 12:12:21 +0100

    added padding

--- frontend/components/NotVerified.tsx
+++ frontend/components/NotVerified.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotVerified = ({ email }: any) => {
   return (
     <div className="flex items-center lg:flex-row flex-col justify-center bg-red-100 py-4 gap-3">
-      <p className="text-sm">
+      <p className="text-sm lg:px-0 px-2">
         To publish your profile please verify your account by clicking the link
         we’ve sent to your email ({email}).
       </p>
