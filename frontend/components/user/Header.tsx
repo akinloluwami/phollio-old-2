c13794b35552facb763680dc86ff6dd27243be00
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-16 19:48:56 +0100

    centered the profile image

--- frontend/components/user/Header.tsx
+++ frontend/components/user/Header.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div className="w-full">
-      <div className="h-36 w-36 bg-accent rounded-full"></div>
+      <center>
+        <div className="h-36 w-36 bg-accent rounded-full"></div>
+      </center>
     </div>
   );
 };
