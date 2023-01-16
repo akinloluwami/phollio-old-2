25b860804e918c4f24fae7b76145cfeb69ddb858
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-16 20:02:22 +0100

    added tabs array

--- frontend/components/user/Tabs.tsx
+++ frontend/components/user/Tabs.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const Tabs = () => {
+  const tabs = [
+    {
+      id: 1,
+      title: "Projects",
+    },
+    {
+      id: 2,
+      title: "Links",
+    },
+    {
+      id: 3,
+      title: "Tech stack",
+    },
+  ];
+
   return (
     <div className="w-full mx-auto">
       <div className="flex items-center justify-center"></div>
