b9bdc6b382180982391e0fd91243546fce156742
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-16 19:57:41 +0100

    imported and rendered tabs comp

--- frontend/pages/[username]/index.tsx
+++ frontend/pages/[username]/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../../components/user/Header";
+import Tabs from "../../components/user/Tabs";
 
 const User = () => {
   return (
     <div className="max-w-2xl mx-auto py-8">
       <Header />
+      <Tabs />
     </div>
   );
 };
