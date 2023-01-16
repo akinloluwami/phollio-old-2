337068fe76a32ff3f7253c0c0c9c4bd55875a2e2
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-16 19:48:24 +0100

    w-h

--- frontend/components/user/Header.tsx
+++ frontend/components/user/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="w-full">Header</div>;
+  return (
+    <div className="w-full">
+      <div className="h-36 w-36 bg-accent rounded-full"></div>
+    </div>
+  );
 };
 
 export default Header;
