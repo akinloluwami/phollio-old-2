3165a9b729c59c62bf8563819b167edad1bd1b9f
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-16 20:01:14 +0100

    >>>

--- frontend/components/user/Tabs.tsx
+++ frontend/components/user/Tabs.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Tabs = () => {
-  return <div className="w-full mx-auto">Tabs</div>;
+  return (
+    <div className="w-full mx-auto">
+      <div className="flex items-center justify-center"></div>
+    </div>
+  );
 };
 
 export default Tabs;
