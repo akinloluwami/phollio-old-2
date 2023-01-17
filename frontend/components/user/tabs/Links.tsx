ffd0612b053ea934b63aa1d8d0c2d859c561a444
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 14:36:35 +0100

    mapped

--- frontend/components/user/tabs/Links.tsx
+++ frontend/components/user/tabs/Links.tsx
@@ -4,9 +4,9 @@ import Link from "../Link";
 const Links = ({ links }: any) => {
   return (
     <div>
-      {links.map((link: any) => {
-        <Link url={link.url} title={link.title} />;
-      })}
+      {links.map((link: any) => (
+        <Link url={link.url} title={link.title} />
+      ))}
     </div>
   );
 };
