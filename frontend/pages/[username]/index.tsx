77a17db98a1180dd64df69e6461c11f35a2ce61d
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 16:10:54 +0100

    not found

--- frontend/pages/[username]/index.tsx
+++ frontend/pages/[username]/index.tsx
@@ -15,17 +15,25 @@ export async function getServerSideProps({ query }: any) {
 }
 
 const User = ({ data }: any) => {
+  const userData = data.data;
   return (
     <>
-      <Head>
-        <title>
-          {data.data.displayName} | {data.data.bio} | Phollio
-        </title>
-      </Head>
-      <div className="max-w-2xl mx-auto py-8">
-        <Header displayName={data.data.displayName} bio={data.data.bio} />
-        <Tabs projects={data.data.projects} links={data.data.links} />
-      </div>
+      {!userData ? (
+        <>404</>
+      ) : (
+        <>
+          <Head>
+            <title>
+              {userData?.displayName || "Phollio"} | {userData?.bio || ""} |
+              Phollio
+            </title>
+          </Head>
+          <div className="max-w-2xl mx-auto py-8">
+            <Header displayName={userData.displayName} bio={userData.bio} />
+            <Tabs projects={userData?.projects} links={userData?.links} />
+          </div>
+        </>
+      )}
     </>
   );
 };
