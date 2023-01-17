b99779d421cb1d3835efb2598e958c4a8804df57
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 13:43:06 +0100

    fetched data from server

--- frontend/pages/[username]/index.tsx
+++ frontend/pages/[username]/index.tsx
@@ -1,7 +1,20 @@
 import Header from "../../components/user/Header";
 import Tabs from "../../components/user/Tabs";
 
-const User = () => {
+export async function getServerSideProps({ query }: any) {
+  const { username } = query;
+  const res = await fetch(`http://localhost:2004/user?username=${username}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const User = ({ data }: any) => {
+  console.log(data);
   return (
     <div className="max-w-2xl mx-auto py-8">
       <Header />
