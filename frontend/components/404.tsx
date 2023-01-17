813771f9a95b2fd16fc6e33a6c90ffc49f7df52d
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 16:14:20 +0100

    styled

--- frontend/components/404.tsx
+++ frontend/components/404.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const User404 = () => {
-  return <div>User404</div>;
+  return (
+    <div className="">
+      <center>
+        <div className="h-20 w-20 bg-accent rounded-full"></div>
+      </center>
+      <h1>The page you're looking for doesn't exist.</h1>
+      <p>
+        Want this to be your username? <a href="#">Create your Phollio now.</a>{" "}
+      </p>
+    </div>
+  );
 };
 
 export default User404;
