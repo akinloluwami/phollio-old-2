8b697274887f09e2c19af234afe41416ef8365e4
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 13:55:08 +0100

    added props

--- frontend/components/user/Header.tsx
+++ frontend/components/user/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({ displayName, bio }: any) => {
   return (
     <div className="w-full">
       <center>
         <div className="h-36 w-36 bg-accent rounded-full"></div>
       </center>
-      <h1 className="text-center font-bold text-3xl my-3">Akinkunmi</h1>
-      <p className="text-center">Software engineer || Solo startup founder</p>
+      <h1 className="text-center font-bold text-3xl my-3">{displayName}</h1>
+      <p className="text-center">{bio}</p>
     </div>
   );
 };
