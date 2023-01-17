42211dd3798bb7f052bb5b2ea08b4f2e4cb6ee4d
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 09:12:39 +0100

    styled

--- frontend/components/user/Project.tsx
+++ frontend/components/user/Project.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <a href="#" className="w-full" target={"_blank"}>
+      <div className="bg-accent py-3 my-3 px-3">
+        <h1>Vibrantt</h1>
+        <p>Color tool for designers and developers</p>
+        <small>vibrantt.co</small>
+      </div>
+    </a>
+  );
 };
 
 export default Project;
