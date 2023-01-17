4959d47cc36fc37dfbcd1c3d7a14607d9489913a
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 09:30:10 +0100

    ><

--- frontend/components/user/Link.tsx
+++ frontend/components/user/Link.tsx
@@ -4,7 +4,7 @@ const Link = () => {
   return (
     <a href="#" className="w-full" target={"_blank"}>
       <div className="bg-slate-200 py-3 my-3 px-3">
-        <h1 className="text-xl">Twitter</h1>
+        <h1 className="text-xl text-center">Twitter</h1>
       </div>
     </a>
   );
--- frontend/components/user/tabs/Links.tsx
+++ frontend/components/user/tabs/Links.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Link from "../Link";
 
 const Links = () => {
-  return <div>Links</div>;
+  return (
+    <div>
+      <Link />
+      <Link />
+      <Link />
+      <Link />
+      <Link />
+      <Link />
+      <Link />
+    </div>
+  );
 };
 
 export default Links;
