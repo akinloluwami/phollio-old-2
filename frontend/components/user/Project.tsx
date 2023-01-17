652f6bbd8e253066577a345e2add08fac300391b
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 14:44:42 +0100

    description

--- frontend/components/user/Project.tsx
+++ frontend/components/user/Project.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Project = () => {
+const Project = ({ id, url, title, description }: any) => {
   return (
-    <a href="#" className="w-full" target={"_blank"}>
+    <a href={description} className="w-full" target={"_blank"}>
       <div className="bg-slate-200 py-3 my-3 px-3">
-        <h1 className="text-xl">Vibrantt</h1>
-        <p>Color tool for designers and developers</p>
-        <small>vibrantt.co</small>
+        <h1 className="text-xl">{title}</h1>
+        <p>{description}</p>
+        <small>{description}</small>
       </div>
     </a>
   );
