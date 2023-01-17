e748106147ff6ab9b36db63fe74e9c840710994e
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 14:46:32 +0100

    url

--- frontend/components/user/Project.tsx
+++ frontend/components/user/Project.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Project = ({ id, url, title, description }: any) => {
   return (
-    <a href={description} className="w-full" target={"_blank"}>
+    <a href={url} className="w-full" target={"_blank"}>
       <div className="bg-slate-200 py-3 my-3 px-3">
         <h1 className="text-xl">{title}</h1>
         <p>{description}</p>
-        <small>{description}</small>
+        <small>{url}</small>
       </div>
     </a>
   );
