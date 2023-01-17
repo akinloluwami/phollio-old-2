217296d7567bc7a3335cca3febb42c1d57fd6e71
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 11:49:42 +0100

    returned user links

--- backend/src/controllers/user/get-user.ts
+++ backend/src/controllers/user/get-user.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import Link from "../../schema/link";
 import User from "../../schema/user";
 
 const getUser = async (req: Request, res: Response) => {
@@ -9,6 +10,20 @@ const getUser = async (req: Request, res: Response) => {
       message: "User not found",
     });
   }
+  if (!UserData.isEmailVerified) {
+    return res.status(400).json({
+      message: "This profile is not yet verified",
+    });
+  }
+  const userId = UserData._id;
+
+  const userLinks = await Link.find({ userId: userId });
+
+  const data = {
+    username: UserData.username,
+  };
+
+  return res.status(200).json({ data });
 };
 
 export default getUser;
