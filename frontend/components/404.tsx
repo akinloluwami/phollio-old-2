664da7ecbfdbc98536897604a2fe2e56b629e8c9
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-17 16:17:26 +0100

    styling...

--- frontend/components/404.tsx
+++ frontend/components/404.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 const User404 = () => {
   return (
-    <div className="">
+    <div className="mt-20">
       <center>
-        <div className="h-20 w-20 bg-accent rounded-full"></div>
+        <div className="h-32 w-32 bg-accent rounded-full"></div>
       </center>
-      <h1>The page you're looking for doesn't exist.</h1>
-      <p>
-        Want this to be your username? <a href="#">Create your Phollio now.</a>{" "}
+      <h1 className="text-center text-2xl my-10">
+        The page you're looking for doesn't exist.
+      </h1>
+      <p className="text-center">
+        Want this to be your username?{" "}
+        <a href="#" className="underline">
+          Create your Phollio now.
+        </a>{" "}
       </p>
     </div>
   );
