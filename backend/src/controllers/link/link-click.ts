32d6f8677636265ced9b89199bf09341cf72d89c
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 15:52:29 +0100

    res

--- backend/src/controllers/link/link-click.ts
+++ backend/src/controllers/link/link-click.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import LinkClick from "../../schema/link-click";
 
 const linkClick = async (req: Request, res: Response) => {
-  res.send("Hey durling!");
+  const {
+    userAgent,
+    ipAddress,
+    deviceType,
+    browser,
+    linkId,
+    visitedAccountId,
+  } = req.body;
+
+  const linkClickData = new LinkClick({
+    userAgent,
+    ipAddress,
+    deviceType,
+    browser,
+    linkId,
+    visitedAccountId,
+  });
+
+  await linkClickData.save();
 };
 
 export default linkClick;
