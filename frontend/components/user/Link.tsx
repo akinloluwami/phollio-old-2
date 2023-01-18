0c2ac4609eee920a2d3a8c746dede2d795da2b78
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 15:52:21 +0100

    states

--- frontend/components/user/Link.tsx
+++ frontend/components/user/Link.tsx
@@ -1,6 +1,16 @@
+import { postData } from "../../utils/requests";
+import { NextApiRequest, NextApiResponse } from "next";
+import { useEffect, useState } from "react";
+
 const Link = ({ id, title, url }: any) => {
+  const [ipAddress, setIpAddress] = useState("");
+  const [deviceType, setDeviceType] = useState("");
+  const [browser, setBrowser] = useState("");
+  const [status, setStatus] = useState("");
+  useEffect(() => {}, []);
+
   const handleClick = () => {
-    console.log(id);
+    postData("/link/click", {}, {}).then((data) => console.log(data));
   };
 
   const linkUrl = url.includes("http://") ? url : `https://${url}`;
