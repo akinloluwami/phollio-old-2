1391b2922651b0178e214adb2aefe9f9946779e4
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 10:16:55 +0100

    created unverified profile component

---
+++ frontend/components/UserNotVerified.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserNotVerified = () => {
+  return (
+    <div className="mt-20">
+      <center>
+        <div className="h-32 w-32 bg-accent rounded-full"></div>
+      </center>
+      <h1 className="text-center text-2xl my-10">
+        This profile is not yet verified, please check back later.
+      </h1>
+    </div>
+  );
+};
+
+export default UserNotVerified;
