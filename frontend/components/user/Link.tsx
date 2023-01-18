91beedec87aeefe3356545db70f39b514c99d9a0
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 15:15:34 +0100

    create click handler

--- frontend/components/user/Link.tsx
+++ frontend/components/user/Link.tsx
@@ -1,7 +1,16 @@
 const Link = ({ id, title, url }: any) => {
+  const handleClick = () => {
+    console.log(id);
+  };
+
   const linkUrl = url.includes("http://") ? url : `https://${url}`;
   return (
-    <a href={linkUrl} className="w-full" target={"_blank"}>
+    <a
+      href={linkUrl}
+      className="w-full"
+      target={"_blank"}
+      onClick={handleClick}
+    >
       <div className="bg-slate-200 py-3 my-3 px-3">
         <h1 className="text-xl text-center">{title}</h1>
       </div>
