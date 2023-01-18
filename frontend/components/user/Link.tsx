944e49856239257e5eacf9aa9639813e571cd622
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 12:15:09 +0100

    fixed link url

--- frontend/components/user/Link.tsx
+++ frontend/components/user/Link.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Link = ({ title, url }: any) => {
+  const linkUrl = url.includes("http://") ? url : `https://${url}`;
   return (
-    <a href={url} className="w-full" target={"_blank"}>
+    <a href={linkUrl} className="w-full" target={"_blank"}>
       <div className="bg-slate-200 py-3 my-3 px-3">
         <h1 className="text-xl text-center">{title}</h1>
       </div>
