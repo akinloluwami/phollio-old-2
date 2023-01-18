01d0ad05cff169f112eeb90621d13204bf32230b
Akinkunmi <akinkunmiisrael41@gmail.com>
2023-01-18 15:36:04 +0100

    routed

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -2,5 +2,6 @@ import addLink from "./link/add-new-link";
 import toggleLink from "./link/toggle-link";
 import deleteLink from "./link/delete-link";
 import getLinks from "./link/get-links";
+import linkClick from "./link/link-click";
 
-export { addLink, toggleLink, deleteLink, getLinks };
+export { addLink, toggleLink, deleteLink, getLinks, linkClick };
